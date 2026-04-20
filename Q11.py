e934d15fd43d04c68a10a1df4bc54d0e0dab2b98
footmessithanos <45325637+footmessithanos@users.noreply.github.com>
2026-04-20 17:35:36 +0530

    Fix typo in comment about array search

--- Q11.py
+++ Q11.py
@@ -1,4 +1,4 @@
-#You are given an array A of size N and an element X. Your task is to find whether the array A contains the element X or not.
+#You are given an array A of size N and an element X, Your task is to find whether the array A contains the element X or not.
 
 
 
