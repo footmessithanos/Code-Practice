a13939969dd31b111617e73e27d3a0dd0975eda6
footmessithanos <45325637+footmessithanos@users.noreply.github.com>
2026-04-22 17:00:08 +0530

    Fix comment formatting in Q31.py

--- Q31.py
+++ Q31.py
@@ -1,5 +1,5 @@
 #Chef is given two integers A and C such that A≤C. Chef wants to find whether there exists any integer B such that A,B, and C are in arithmetic progression.
-#For each test case, output −1 if there exists no integer B such that A,B, and C are in arithmetic progression. Else, output the value of B.
+#For each test case, output −1 if there exists no integer B such that A, B, and C are in arithmetic progression. Else then output the value of B.
 
 
 
